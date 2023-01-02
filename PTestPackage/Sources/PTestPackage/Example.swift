f16b1aa264e882b8e42cf41a0d642bb61a04a128
Bill Wake <william.wake@acm.org>
2023-01-02 12:17:33 -0600

    Slightly revised version of ExampleTests

---
+++ PTestPackage/Sources/PTestPackage/Example.swift
@@ -0,0 +1,34 @@
+//
+//  File.swift
+//  
+//
+//  Created by Bill Wake on 1/2/23.
+//
+
+import Foundation
+
+struct Example<Input, Output> {
+  var input: Input
+  var output : Output
+  var message: String
+  var line: Int
+  
+  init(input: Input, output: Output,
+       _ message: String = "", _ line: Int = #line) {
+    self.input = input
+    self.output = output
+    self.message = message
+    self.line = line
+  }
+  
+  func msg() -> String {
+    return "Line \(line): \(message)"
+  }
+}
+
+func checkTestCases<Input, Output>(
+  _ tests: [Example<Input, Output>],
+  _ parameterizedAssert: (Example<Input, Output>) -> ())
+{
+  tests.forEach { parameterizedAssert($0) }
+}
---
+++ PTestPackage/Tests/PTestPackageTests/ExampleTests.swift
@@ -0,0 +1,30 @@
+//
+//  ExampleTests.swift
+//  
+//
+//  Created by Bill Wake on 1/2/23.
+//
+
+import XCTest
+@testable import PTestPackage
+
+class Demo {
+  func stringOfSum(_ a: Int, _ b: Int) -> String {
+    return "\(a + b)"
+  }
+}
+
+final class ExampleTests: XCTestCase {
+  func testStringOfSum() {
+    checkTestCases(
+    [
+      Example(input: (-1, 1), output: "0", "zero"),
+      Example(input: (3, 0), output: "3", "one-digit"),
+      Example(input: (-2, 1), output: "-1", "negative")
+    ]) { p in
+      let my = Demo()
+      let actual = my.stringOfSum(p.input.0, p.input.1)
+      XCTAssertEqual(p.output, actual, p.msg())
+    }
+  }
+}
