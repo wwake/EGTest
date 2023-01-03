f30e48bf9baea2b77579fa944a7c2c3f58e6ab2d
Bill Wake <william.wake@acm.org>
2023-01-03 11:55:48 -0600

    Spike of resultBuilder

--- PTestPackage/Sources/PTestPackage/EG.swift
+++ PTestPackage/Sources/PTestPackage/EG.swift
@@ -34,3 +34,55 @@ struct Check {
     tests.forEach { parameterizedAssert($0) }
   }
 }
+
+struct Test<Input, Output> {
+  let assert: ((EG<Input, Output>) -> ())
+  let examples: [EG<Input, Output>]
+}
+
+@resultBuilder struct Check2<Input, Output> {
+  
+  static public func buildBlock( 
+    _ components: EG<Input, Output>... 
+  ) -> [EG<Input,Output>] {
+    Array(components)
+  }
+  
+//  static public func buildExpression(_ expression: EG<Input, Output>) -> 
+//  EG<Input, Output> {
+//    expression
+//  }
+}
+
+@resultBuilder struct Check3<Input, Output> {
+  
+  static public func buildBlock( 
+    _ component: @escaping Assert<Input,Output>) 
+  -> Assert<Input,Output> {
+    return component
+  }
+  
+//  static public func buildExpression(_ expression: @escaping Assert<Input,Output>) -> 
+//  Assert<Input, Output> {
+//    expression
+//  }
+}
+
+typealias Assert<Input,Output> = (EG<Input,Output>) -> ()
+
+//func myAsserter2<Input, Output>(_ assert: Assert<Input,Output>) {
+//  
+//}
+//
+//func myAsserter<Input, Output>(examples: [EG<Input, Output>]) -> ((Assert<Input,Output>) -> ()) {
+//  
+//}
+
+func Examples<Input,Output>(@Check2<Input,Output> _ content: () -> [EG<Input, Output>], _ assert: Assert<Input,Output>) -> () {
+  Check.examples(content(), assert)
+}
+
+
+func Checking<Input,Output>(@Check3<Input,Output>  content: @escaping Assert<Input,Output>) -> (Assert<Input, Output>) {
+  return content
+}
