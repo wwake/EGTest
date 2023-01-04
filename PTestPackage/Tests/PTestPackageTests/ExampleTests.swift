90b598c8afa38954c85dfd9fa115cd03206c26fb
Bill Wake <william.wake@acm.org>
2023-01-04 12:48:18 -0600

    Remove failed resultBuilder and AAA attempts

--- PTestPackage/Tests/PTestPackageTests/ExampleTests.swift
+++ PTestPackage/Tests/PTestPackageTests/ExampleTests.swift
@@ -28,40 +28,4 @@ final class ExampleTests: XCTestCase {
       }
   }
   
-  func testStringOfSum2() {
-    Examples {
-      eg(input: (-1, 1), output: "0", "zero")
-      eg(input: (3, 0), output: "3", "one-digit")
-      eg(input: (-2, 1), output: "-1", "negative")
-    }.check { p in
-      let my = Demo()
-      let actual = my.stringOfSum(p.input.0, p.input.1)
-      XCTAssertEqual(p.output, actual, p.msg())
-    }
-  }
-  
-  func testAAA_original() {
-    let subject = Arrange2 {
-      Demo()
-    }
-    
-    let actual = Act2(subject) {
-      $0.stringOfSum(3, 4)
-    }
-    
-    Assert2(actual) {
-      eq($0, "7") 
-      && $0 > "1"
-    }
-  }
-  
-//  func testAAA() {
-//    Testing {
-//      Arrange { Demo() }
-//      
-//      Act<Demo,String>{ $0.stringOfSum(3, 4) }
-//      
-//      Assert { $0 == "7" }
-//    }
-//  }
 }
